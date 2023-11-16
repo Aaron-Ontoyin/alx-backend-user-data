316d4d5294202206360e5632d3f73c967a51c95b
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-11-16 23:07:02 +0000

    10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -49,3 +49,13 @@ class Auth:
         except NoResultFound:
             pass
         return False
+
+    def create_session(self, email: str) -> str:
+        """Create a new session for the user with `email`"""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return

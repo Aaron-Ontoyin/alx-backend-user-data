1ab799be8b60176340b9b50396cb34fc92a5c0a5
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-11-16 21:23:29 +0000

    pycodestyle

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -13,6 +13,6 @@ class User(Base):
 
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
     email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), Sequence('user_id_seq'), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=False)
     reset_token = Column(String(250), nullable=False)

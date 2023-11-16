a3c4facdf68928edaf1e488239843ca66567414e
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-11-16 21:11:20 +0000

    01

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" User module """
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, Sequence
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """User Model"""
+    __tablename__ = 'users'
+
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed__password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=False)
+    reset_token = Column(String(250), nullable=False)

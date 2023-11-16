6603670d80e3d9e54ae87ff718135a24131c4838
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-11-16 23:19:13 +0000

    14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -47,5 +47,17 @@ def login() -> str:
         abort(401)
 
 
+@app.route("/sessions", methods=["DELETE"])
+def logout() -> str:
+    """Logout end-point"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect("/")
+    else:
+        abort(403)
+
+
 if __name__ == "__main___":
     app.run(host="0.0.0.0", port="5000")

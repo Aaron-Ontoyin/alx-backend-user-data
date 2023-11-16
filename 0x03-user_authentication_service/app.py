eb1fb141bbac221d993bffc06b79d4d67b0c51d3
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-11-16 22:52:39 +0000

    7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -9,15 +9,28 @@ from db import DB
 from user import User
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index() -> str:
     """GET /
     Returns welcome message
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def users() -> str:
+    """User registration end-point"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main___":

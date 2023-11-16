7010fbd4c441e0e2b14f19ade636be62853c4ccb
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-11-16 22:29:29 +0000

    self

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,8 +3,17 @@
 Authentication module
 """
 import bcrypt
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from db import DB
+from user import User
+import uuid
 
 
-def _hash_password(self, password: str) -> bytes:
+
+def _hash_password(password: str) -> bytes:
     """Return a salted hash of the input password"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())

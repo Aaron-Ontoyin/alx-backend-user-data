5d3cef1b1e268ddf8931000795d4b3df8128c409
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-11-16 23:11:36 +0000

    11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -33,5 +33,19 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route("/sessions", methods=["POST"])
+def login() -> str:
+    """Login end-point"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = make_response({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
+
+
 if __name__ == "__main___":
     app.run(host="0.0.0.0", port="5000")

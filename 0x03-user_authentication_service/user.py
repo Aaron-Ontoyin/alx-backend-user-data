570e5dbd192e503d32ac1cbab7d72c13ff329104
Aaron-Ontoyin <aarononto90@gmail.com>
2023-11-16 21:16:13 +0000

    seq

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -11,7 +11,7 @@ class User(Base):
     """User Model"""
     __tablename__ = 'users'
 
-    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed__password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=False)

d2be9738acbce59eb14b0ef2a890654af737b0b8
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-11-16 22:43:47 +0000

    6

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+from flask import Flask, jsonify, request, abort, redirect
+from flask.helpers import make_response
+from auth import Auth
+from db import DB
+from user import User
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index() -> str:
+    """GET /
+    Returns welcome message
+    """
+    return jsonify({"message": "Bienvenue"})
+
+if __name__ == "__main___":
+    app.run(host="0.0.0.0", port="5000")
